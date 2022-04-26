093ec9cc77adbb68c08c8467560c85a625fb1d9d
pingjun <pingjun@eastmoney.com>
2022-04-26 16:33:21 +0800

    build: 更新配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  bracketSpacing: true,
-  singleQuote: false,
-  arrowParens: "avoid",
-  trailingComma: "none"
+  bracketSpacing: true, // 在对象，数组括号与文字之间加空格 “{ foo: bar }”
+  singleQuote: false, // 是否使用单引号
+  arrowParens: "avoid", // (x) => {} 箭头函数参数只有一个时是否要有小括号。avoid：省略括号
+  trailingComma: "none" // 在对象或数组最后一个元素后面是否添加逗号
 };
